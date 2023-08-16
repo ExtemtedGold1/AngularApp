79e398538a44524a6eb6aaadec549a3e9cff9f18
ExtemtedGold1 <wiktorbielecki31@gmail.com>
2023-08-16 18:00:22 +0200

    List page template and logic

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './home/home.component';
 import { AddFunctionalityComponent } from './add-functionality/add-functionality.component';
 import { EditFunctionalityComponent } from './edit-functionality/edit-functionality.component';
 import { ViewFunctionalityComponent } from './view-functionality/view-functionality.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { ViewFunctionalityComponent } from './view-functionality/view-functional
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularApp/src/app/home/home.component.ts
+++ AngularApp/src/app/home/home.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Type } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { HttpProviderService } from '../Service/http-provider.service';
+import { IfStmt } from '@angular/compiler';
+
+
+@Component({
+  selector: 'ng-modal-confrim',
+  template: `
+  <div class="modal-header">
+    <h5 class="modal-title" id="modal-title">Delete Confirmation</h5>
+    <button type="button" class="btn close" aria-label="Close button" aria-describedby="modal-title" (click)="modal.dismiss('Cross click')">
+      <span aria-hidden="true">×</span>
+    </button>
+  </div>
+  <div class="modal-body">
+    <p>Are you sure you want to delete?</p>
+  </div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-outline-secondary" (click)="modal.dismiss('cancel click')">CANCEL</button>
+    <button type="button" ngbAutofocus class="btn btn-success" (click)="modal.close('Ok click')">OK</button>
+  </div>
+  `,
+  //templateUrl: './home.component.html',
+  //styleUrls: ['./home.component.scss']
+})
+export class NgModalConfirm {
+  constructor(public modal: NgbActiveModal) {}
+}
+const MODALS: { [name: string]: Type<any> } = {
+  deleteModal: NgModalConfirm,
+};
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit {
+  closeResault = '';
+  functionalityList: any = [];
+  constructor(private router: Router, private modalServices: NgbModal, private toastr: ToastrService, private httpProvider: HttpProviderService) {}
+
+  ngOnInit(): void {
+    this.getAllFunctionality();
+  }
+  async getAllFunctionality() {
+    this.httpProvider.getAllFunctionality().subscribe((data: any) => {
+      if(data != null && data.body != null) {
+        var resultData = data.body;
+        if(resultData) {
+          this.functionalityList = resultData;
+        }
+      }
+    },
+    (error: any) => {
+      if(error) {
+        if(error.status == 404) {
+          if(error.error && error.error.message){
+            this.functionalityList = [];
+          }
+        }
+      }
+    });
+  }
+  AddFunctionality() {
+    this.router.navigate(['AddFunctionality']);
+  }
+
+  deleteFunctionalityConfirmation(functionality: any){
+    this.modalServices.open(MODALS['deleteModal'],
+    {
+      ariaLabelledBy: 'modal-basic-title'
+    }).result.then((result) => {
+      this.deleteFunctionality(functionality);
+    },
+    (reason) => {});
+  }
+
+  deleteFunctionality(functionality: any) {
+    this.httpProvider.deleteFunctionalityById(functionality.id).subscribe((data: any) => {
+      if(data != null && data.body != null) {
+        var resultData = data.body;
+        if(resultData != null && resultData.isSuccess) {
+          this.toastr.success(resultData.message);
+          this.getAllFunctionality();
+        }
+      }
+    },
+    (error:any) => {});
+  }
+
+
+  
 
 }
