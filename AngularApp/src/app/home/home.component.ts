4a224cd5e37919e8fafa57bf480e4014d35f2a69
ExtemtedGold1 <wiktorbielecki31@gmail.com>
2023-08-18 21:48:12 +0200

    fix components

--- AngularApp/src/app/home/home.component.ts
+++ AngularApp/src/app/home/home.component.ts
@@ -22,30 +22,40 @@ const MODALS: { [name: string]: Type<any> } = {
 export class HomeComponent implements OnInit {
   closeResault = '';
   functionalityList: any = [];
-  constructor(private router: Router, private modalServices: NgbModal, private toastr: ToastrService, private httpProvider: HttpProviderService) {}
+  constructor(
+    private router: Router,
+    private modalServices: NgbModal,
+    private toastr: ToastrService,
+    private httpProvider: HttpProviderService) {}
 
   ngOnInit(): void {
-    let functionality = [{
-      Nazwa: "Szybka Rezerwacja",
+    this.functionalityList = [{
+      Name: "Szybka Rezerwacja",
       Description: "Umożliwia klientom dokonanie natychmiastowej rezerwacji usług lub produktów bez konieczności rejestracji, zwiększając konwersje i usprawniając proces zakupowy.",
       Type: "Usprawnienie procesu zakupowego.",
       Note: "Brak przypisu do Kanbana"
     },
     {
-      Nazwa: "Indywidualne Rekomendacje",
+      Name: "Indywidualne Rekomendacje",
       Description: "Wykorzystuje algorytmy uczenia maszynowego do analizy zachowań klientów i dostarczania spersonalizowanych rekomendacji produktów lub treści, zwiększając zaangażowanie i sprzedaż.",
       Type: "Udoskonalenie obsługi klienta.",
       Note: "Brak przypisu do Kanbana"
     },
     {
-      Nazwa: "Wirtualne Przymierzanie",
+      Name: "Wirtualne Przymierzanie",
       Description: "Tworzy wirtualne środowisko, w którym klienci mogą przymierzyć odzież lub akcesoria za pomocą rzeczywistości rozszerzonej, poprawiając interakcję ze sklepem internetowym i redukując zwroty.",
       Type: "Innowacyjne rozwiązanie technologiczne.",
       Note: "Brak przypisu do Kanbana"
+    },
+    {
+      Name: "Edit",
+      Description: "Edit",
+      Type: "Edit",
+      Note: "Edit"
     }];
   };
   
-  AddFunctionality() {
+  goToAddFunctionality() {
     this.router.navigate(['AddFunctionality']);
   }
 
